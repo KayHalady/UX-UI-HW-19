15beaeab8a7d9ceadca34a6474382268a21aefbc
KayHalady <kedar.halady@gmail.com>
2022-08-11 18:34:53 -0400

    commit
    updated loading function java

--- js/index.js
+++ js/index.js
@@ -13,4 +13,4 @@ function blurring() {
   bgColour.style.opacity = scale(load, 0, 100, 1, 0)
 }
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min}
